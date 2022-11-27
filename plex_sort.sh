faf1f090a8149e06251e1decd06f122e0c9a0b7a
GUY Jérôme <scoonydeus@gmail.com>
2022-11-27 10:51:51 +0100

    Update plex_sort.sh

--- plex_sort.sh
+++ plex_sort.sh
@@ -33,7 +33,6 @@ if curl -s -m 3 --head --request GET https://github.com > /dev/null; then
       chmod +x /opt/scripts/plex_sort.sh
       echo "Update Completed, restart script"
       exit 1
-      bash $0
     }
     script_upgrade
   fi
